751569b58f8c04d0b7e70eac19cff87926862d07
SASB9114 <92628191+SASB9114@users.noreply.github.com>
2022-01-14 18:48:45 +0200

    main
    main

---
+++ Sas_Bogdan_Lab10/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace Sas_Bogdan_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
